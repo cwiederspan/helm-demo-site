7fe7540d27d987dbe1bbda418477797bd2c478ae
Chris Wiederspan <chwieder@microsoft.com>
2018-08-09 17:20:59 -0600

    Add project files.

---
+++ Helm.Site/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Helm.Site.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
